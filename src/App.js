eacb13864d3d694aa81dc842457e33deb1ec3eec
Aayushi Mittal <aayushimittal088@gmail.com>
2022-05-04 22:53:18 +0530

    Add components and pages folder with react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CreateEvent from './pages/CreateEvent'
+import Vote from './pages/Vote'
+import Results from './pages/Results'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} /> 
+          <Route path="create-event" element={<CreateEvent />} />
+          <Route path="vote" element={<Vote />} />
+          <Route path="results" element={<Results />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
